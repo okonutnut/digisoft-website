140192022c9df78ac74a18d76213cfd21bed724d
okonutnut <okonutnut@users.noreply.github.com>
2025-03-03 12:32:50 +0000

    deploy: f06432bc74ddd9fac42d7a8873972be02721d907

---
+++ assets/footer-8va3nIXz.js
@@ -0,0 +1,16 @@
+import{j as e,L as i}from"./index-BNqiFzP2.js";import{c as s,G as n,a as o}from"./read-excel-D3Uh9pMj.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z",key:"1jg4f8"}]],x=s("Facebook",r);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["rect",{width:"20",height:"16",x:"2",y:"4",rx:"2",key:"18n3k1"}],["path",{d:"m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7",key:"1ocrg3"}]],h=s("Mail",d);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=[["path",{d:"M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z",key:"foiqr5"}]],j=s("Phone",m);function b({className:c}){const t=n();return e.jsx("footer",{className:"w-full text-current flex flex-col justify-start items-center",children:e.jsxs("section",{className:o(c,"w-full"),children:[e.jsxs("div",{className:"h-full w-full flex justify-around gap-4 p-4",children:[e.jsxs("div",{className:"block xs:hidden md:block lg:block xl:block 2xl:block",children:[e.jsx("label",{className:"uppercase font-bold tracking-widest",children:"products"}),e.jsx("ul",{children:t.map((l,a)=>e.jsx("li",{className:"mb-1",children:e.jsx("a",{href:l.href,children:l.code})},a))})]}),e.jsxs("div",{className:"col-span-full",children:[e.jsx("label",{className:"uppercase font-bold tracking-widest xs:text-sm",children:"FOR MORE INFORMATION CONTACT"}),e.jsxs("div",{className:"mb-5 xs:text-xs",children:[e.jsx("h1",{className:"mt-3 font-semibold",children:"THOMAS C. SADUL"}),e.jsx("h4",{children:"President / CEO / Chief Architect"})]}),e.jsxs("ul",{className:"xs:text-xs",children:[e.jsxs("li",{className:"flex items-center gap-2 mb-1",children:[e.jsx(h,{}),e.jsx("span",{children:"tcsadul@yahoo.com"})]}),e.jsxs("li",{className:"flex items-center gap-2 mb-1",children:[e.jsx(j,{}),e.jsx("span",{children:"Globe: 09278591168 | Smart: 09214524212"})]}),e.jsxs("li",{className:"flex items-center gap-2 mb-1",children:[e.jsx(x,{}),e.jsx(i,{to:"https://www.facebook.com/DigiSoftPH/",target:"_blank",children:"DigiSoftPH"})]})]})]})]}),e.jsxs("div",{className:"text-sm text-center pb-2 xs:text-[12px]",children:[e.jsx("p",{children:"All Rights Reserved 2003-2019"}),e.jsx("p",{children:"Digital Software © 2003-2019"})]})]})})}export{b as default};
---
+++ assets/hamburger-menu-DXxMLYuF.js
@@ -0,0 +1,16 @@
+import{j as e,L as i}from"./index-BNqiFzP2.js";import{c as s,G as n,$ as c,a0 as l,B as r,a1 as h}from"./read-excel-D3Uh9pMj.js";import{A as o,a as m,b as p,c as x}from"./accordion-BS9rxzn2.js";import"./index-DQWJVhl9.js";import"./chevron-down-BYZGQOYW.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=[["path",{d:"M3 12h18",key:"1i2n21"}],["path",{d:"M3 18h18",key:"1h113x"}],["path",{d:"M3 6h18",key:"d0wm0j"}]],k=s("AlignJustify",y);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const j=[["path",{d:"M2.97 12.92A2 2 0 0 0 2 14.63v3.24a2 2 0 0 0 .97 1.71l3 1.8a2 2 0 0 0 2.06 0L12 19v-5.5l-5-3-4.03 2.42Z",key:"lc1i9w"}],["path",{d:"m7 16.5-4.74-2.85",key:"1o9zyk"}],["path",{d:"m7 16.5 5-3",key:"va8pkn"}],["path",{d:"M7 16.5v5.17",key:"jnp8gn"}],["path",{d:"M12 13.5V19l3.97 2.38a2 2 0 0 0 2.06 0l3-1.8a2 2 0 0 0 .97-1.71v-3.24a2 2 0 0 0-.97-1.71L17 10.5l-5 3Z",key:"8zsnat"}],["path",{d:"m17 16.5-5-3",key:"8arw3v"}],["path",{d:"m17 16.5 4.74-2.85",key:"8rfmw"}],["path",{d:"M17 16.5v5.17",key:"k6z78m"}],["path",{d:"M7.97 4.42A2 2 0 0 0 7 6.13v4.37l5 3 5-3V6.13a2 2 0 0 0-.97-1.71l-3-1.8a2 2 0 0 0-2.06 0l-3 1.8Z",key:"1xygjf"}],["path",{d:"M12 8 7.26 5.15",key:"1vbdud"}],["path",{d:"m12 8 4.74-2.85",key:"3rx089"}],["path",{d:"M12 13.5V8",key:"1io7kd"}]],u=s("Boxes",j);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=[["path",{d:"M10 12h11",key:"6m4ad9"}],["path",{d:"M10 18h11",key:"11hvi2"}],["path",{d:"M10 6h11",key:"c7qv1k"}],["path",{d:"M4 10h2",key:"16xx2s"}],["path",{d:"M4 6h1v4",key:"cnovpq"}],["path",{d:"M6 18H4c0-1 2-2 2-3s-1-1.5-2-1",key:"m9a95d"}]],M=s("ListOrdered",f);function b(){const t={title:"Browse Products",items:n()};return e.jsx(e.Fragment,{children:e.jsxs(c,{children:[e.jsx(l,{asChild:!0,children:e.jsx(r,{variant:"ghost",className:"block md:hidden lg:hidden xl:hidden 2xl:hidden",children:e.jsx(k,{className:" h-10 w-10"})})}),e.jsxs(h,{className:"py-10",side:"left",children:[e.jsx(o,{type:"single",collapsible:!0,children:e.jsxs(m,{value:"item-1",className:"border-none",children:[e.jsx(p,{className:"text-sm font-medium",children:e.jsxs("span",{className:"flex items-center gap-2",children:[e.jsx(u,{className:"h-4 w-4"}),t.title]})}),e.jsx(x,{className:"p-2",children:e.jsx("ul",{children:t.items.map((a,d)=>e.jsx("li",{className:"my-1",children:e.jsx(i,{to:a.href,className:"text-xs underline",children:a.title},d)},d))})})]})}),e.jsxs("span",{className:"flex items-center gap-2",children:[e.jsx(M,{className:"h-4 w-4"}),e.jsx(i,{to:"/list-of-clients",className:"text-sm font-medium",children:"List of Clients"})]})]})]})})}export{b as default};
