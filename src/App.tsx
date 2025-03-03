c6a9514f0997d7350428c0e3b4fcd5d0a1232d56
Okonutnut <darlitocabalse.acad@gmail.com>
2025-03-03 20:57:56 +0800

    Add vite-plugin-gh-pages and update routing to use BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { lazy } from "react";
 const HomePage = lazy(() => import("./pages/home/page"));
 const ProductPreview = lazy(() => import("./pages/preview/page"));
@@ -6,12 +6,12 @@ const ListOfClients = lazy(() => import("./pages/listofclients/page"));
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/list-of-clients" element={<ListOfClients />} />
         <Route path="/products/:id" element={<ProductPreview />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import { compression } from 'vite-plugin-compression2'
+import { ghPages } from 'vite-plugin-gh-pages'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  compression({
-    algorithm: "brotliCompress",
-  }),
+  base: "/digisoft-redesign",
+  plugins: [
+    react(),
+    ghPages(),
+    compression({
+      algorithm: "brotliCompress",
+    }),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/digisoft-redesign/",
 })
 
