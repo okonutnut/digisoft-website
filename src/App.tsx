ad4c6e8d15a05b7d53fef5877340bf5863226b64
Okonutnut <darlitocabalse.acad@gmail.com>
2025-03-03 20:50:13 +0800

    Replace BrowserRouter with HashRouter for improved routing compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { lazy } from "react";
 const HomePage = lazy(() => import("./pages/home/page"));
 const ProductPreview = lazy(() => import("./pages/preview/page"));
@@ -6,12 +6,12 @@ const ListOfClients = lazy(() => import("./pages/listofclients/page"));
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/list-of-clients" element={<ListOfClients />} />
         <Route path="/products/:id" element={<ProductPreview />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
