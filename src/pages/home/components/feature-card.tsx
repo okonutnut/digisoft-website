60b984fd05bf6e0b02a30d72972923f6f101397e
Okonutnut <darlitocabalse.acad@gmail.com>
2025-03-11 11:40:03 +0800

    Update FeatureCard component layout and image styling for improved presentation

--- src/pages/home/components/feature-card.tsx
+++ src/pages/home/components/feature-card.tsx
@@ -103,14 +103,16 @@ const FeatureDescription = ({ children }: { children?: React.ReactNode }) => {
 export const SkeletonOne = () => {
   return (
     <div className="relative flex py-8 px-2 gap-10 h-full">
-      <div className="w-full  p-5  mx-auto bg-white dark:bg-neutral-900 shadow-2xl group h-full">
-        <div className="flex flex-1 w-full h-full flex-col space-y-2 relative py-4">
-          {/* TODO */}
-          <img src="/images/list-of-products.png" alt="list-of-produtcs" />
-        </div>
+      <div className="w-full p-5 mx-auto bg-white dark:bg-neutral-900 shadow-2xl group h-full flex justify-center items-center">
+        {/* TODO */}
+        <img
+          src="/images/list-of-products.png"
+          alt="list-of-produtcs"
+          className="object-center"
+        />
       </div>
 
-      <div className="absolute bottom-0 z-40 inset-x-0 h-[150px] bg-gradient-to-t from-white dark:from-black via-white dark:via-black to-transparent w-full pointer-events-none" />
+      <div className="absolute bottom-0 z-40 inset-x-0 h-[120px] bg-gradient-to-t from-white dark:from-black via-white dark:via-black to-transparent w-full pointer-events-none" />
       <div className="absolute top-0 z-40 inset-x-0 h-[150px] bg-gradient-to-b from-white dark:from-black via-transparent to-transparent w-full pointer-events-none" />
     </div>
   );
